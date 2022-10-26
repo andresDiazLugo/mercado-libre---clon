4e0babf67b059488c61827ab44406de836bb86bc
genjutsu <andresd.ad5@gmail.com>
2022-10-26 16:41:47 -0300

    add styles y new functions

---
+++ src/components/Mensajes/Mensajes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './Mensajes.module.css'
+export default function Mensajes() {
+  return (
+    <div className={style.container}>
+        <div className={style.subContainer}>
+            <img src='https://res.cloudinary.com/da76mkk4h/image/upload/v1666809599/lupa_dmap5s.png'/>
+            <div>
+                <h2>No hay publicaciones que coincidan con tu búsqueda.</h2>
+                <ul className={style.subContainerUl}>
+                    <li><span className={style.styleSpan}>Revisá la ortografía </span>Revisá la ortografía de la palabra</li>
+                    <li>Utlizá <span className={style.styleSpan}>palabras más genéricas</span> o menos palabras</li>
+                    <li><span className={style.navega}>Navegá por las categorías</span>  para encontrar un producto simular</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -2,27 +2,39 @@ import {useEffect} from 'react'
 import {useParams}from'react-router-dom'
 import {useSelector,useDispatch} from 'react-redux'
 import {getAllProductsApiMercadoLibre} from '../../redux/slice/sliceProducts'
+import style from './ProductListing.module.css'
+import {convertARS}from './convertARS'
+import Mensajes from '../Mensajes/Mensajes'
 export default function ProductListing() {
     const dispatch = useDispatch();
     const allProducts = useSelector(state=>state.products.allProducts)
+    const msgError = useSelector(state => state.products.msgError)
     const {name} = useParams()
     useEffect(()=>{
         dispatch(getAllProductsApiMercadoLibre(name))
     },[name])
+
+    if(msgError){
+      return(
+        <Mensajes/>
+      )
+    }
+    
   return (
-    <div style={{minHeight:"80vh"}}>
+   <div className={style.container}>
         <ul>
             {allProducts?.map(e=>{
-                return<li key={e.id}>
+                return<li className={style.containerLi} key={e.id}>
                         <img src={e.img}/>
-                        <div>
+                        <div >
                             <h4>{e.title}</h4>
-                            <p>{e.price}</p>
-                            <p>{e.condition}</p>
+                            <p className={style.price}>{"$ " + convertARS(e.price)}</p>
+                            {e.shipping && <p className={style.envio}>Envío gratis</p>}
+                            <p className={style.condition}>{e.condition === "new" ? "Nuevo": "Usado"}</p>
                         </div>
                       </li>
             })}
         </ul>
-    </div>
+  </div>
   )
 }
